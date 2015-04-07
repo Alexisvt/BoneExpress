46b0fca808a73af216d5ae34fd54f5a4351574d4
Alexis <alexisvt@gmail.com>
2015-04-06 21:58:58 -0600

    practice more with models

--- BoneApps/Practice2/app.js
+++ BoneApps/Practice2/app.js
@@ -13,8 +13,38 @@ var initModel = Config.Backbone.Model.extend({
   }
 })
 
+
+var modelListener = Config.Backbone.Model.extend({
+  defaults: {
+    title: "",
+    completed: false
+  },
+  initialize: function(){
+    console.log("This model has been initialized");
+    this.on("change:title", function(){
+      console.log("Title value for this model have changed");
+    });
+    this.on("change:completed", function(){
+      console.log("Completed value for this model have changed");
+    });
+  },
+  setTitle: function(newTitle){
+    this.set({title: newTitle});
+    console.log("The new title is: " + this.get("title"));
+  },
+  setCompleted: function(newCompleted){
+    this.set({completed:newCompleted});
+  }
+})
+
+
 // var getModel= Config.Backbone.Model.extend({
 //
 // })
 
-module.exports= {model: todo, initModel: initModel};
+module.exports= {
+  model: todo,
+  initModel: initModel,
+  modelListener: modelListener,
+  Backbone: Config.Backbone
+  };
